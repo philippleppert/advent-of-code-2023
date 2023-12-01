123d6a8d0941e1a036a7d70a31e4b63d4aaf4948
Philipp Leppert <philippleppert@web.de>
2023-12-01 21:53:25 +0100

    stuck on part 2 of day 1

--- day1/day1.R
+++ day1/day1.R
@@ -4,9 +4,32 @@ library(stringi)
 # data
 data_d1 <- readLines("day1/day1.txt")
 
+# part 1 ----
+
 # digits
 first_digit <- stri_extract_first_regex(data_d1, "\\d{1}")
 last_digit <- stri_extract_last_regex(data_d1, "\\d{1}")
 
 # combine and sum
 str_c(first_digit, last_digit) %>% as.numeric %>% sum
+
+# part 2 ----
+digits_as_string <- set_names(
+  {1:9 %>% as.character}, 
+  c("on", "tw", "thre", "four",
+    "fiv", "six", "seve", "eigh", "nin")
+  )
+
+data_d1_mod <- data_d1  %>% str_replace_all(digits_as_string)
+
+# digits
+first_digit <- stri_extract_first_regex(data_d1_mod, "\\d{1}")
+last_digit <- stri_extract_last_regex(data_d1_mod, "\\d{1}")
+
+# combine and sum
+str_c(first_digit, last_digit) %>% as.numeric %>% sum
+
+# need to fix eightwo and similar cases
+data_d1[748]
+data_d1_mod[748]
+data_d1_mod[748]
