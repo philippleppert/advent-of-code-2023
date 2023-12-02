320adb100dd5d540946cd6142b657f3a4124044b
Philipp Leppert <philippleppert@web.de>
2023-12-02 16:31:29 +0100

    day 1 part 2

--- day1/day1.R
+++ day1/day1.R
@@ -14,13 +14,23 @@ last_digit <- stri_extract_last_regex(data_d1, "\\d{1}")
 str_c(first_digit, last_digit) %>% as.numeric %>% sum
 
 # part 2 ----
+
+## patterns to look for
+pattern <- "[0-9]|(on(?=e))|(tw(?=o))|(thre(?=e))|(four)|(fiv(?=e))|(six)|(seve(?=n))|(eigh(?=t))|(nin(?=e))"
+
+## dict
 digits_as_string <- set_names(
   {1:9 %>% as.character}, 
   c("on", "tw", "thre", "four",
     "fiv", "six", "seve", "eigh", "nin")
   )
 
-data_d1_mod <- data_d1  %>% str_replace_all(digits_as_string)
+## modify
+data_d1_mod <- 
+  data_d1 %>% 
+  str_extract_all(pattern) %>% 
+  map(., ~str_replace_all(.x, digits_as_string)) %>%
+  map_chr(., ~str_c(.x, collapse = ""))
 
 # digits
 first_digit <- stri_extract_first_regex(data_d1_mod, "\\d{1}")
@@ -28,8 +38,3 @@ last_digit <- stri_extract_last_regex(data_d1_mod, "\\d{1}")
 
 # combine and sum
 str_c(first_digit, last_digit) %>% as.numeric %>% sum
-
-# need to fix eightwo and similar cases
-data_d1[748]
-data_d1_mod[748]
-data_d1_mod[748]
