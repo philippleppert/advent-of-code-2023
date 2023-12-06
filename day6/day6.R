3f3d1c1d1bdc929e9a18a43ebf96784624653b8b
Philipp Leppert <philippleppert@web.de>
2023-12-06 20:52:18 +0100

    day 6 part 1

---
+++ day6/day6.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+# part 1 ----
+# data 
+times <- c(40, 82, 91, 66)
+record_distances <- c(277, 1338, 1349, 1063)
+
+# make list with vector of times
+times_list <- map(times, ~0:.x)
+
+# function to determine total distance 
+total_dist <- function(x){
+  (max(x)-x)*x
+}
+
+# map over times_list vectors and calcualte total distance
+total_distance_list <- map(1:4, ~{times_list[[.x]] %>% total_dist()})
+
+# map over total_distance_list and compare with record_distance for each element
+compare_list <- map(1:4, ~which(total_distance_list[[.x]] > record_distances[.x]))
+
+# obtain product
+map(compare_list, ~length(.x)) %>% unlist %>% prod()
